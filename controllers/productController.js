87c56f68b2ff23e48bdb0217c144f8d08a67b0e1
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2022-11-06 15:57:24 +0530

    addproduct api completed

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,9 +2,19 @@ import Product from "../models/productModel.js";
 
 export const getProductController = async (req, res) => {
     try {
-
         const products = await Product.find();
         res.status(200).send(products);
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const addProductController = async (req, res) => {
+    try {
+        const newProducts = new Product(req.body);
+        await newProducts.save();
+        res.status(200).send("Product added successfully!");
 
     } catch(error) {
         console.log(error);
