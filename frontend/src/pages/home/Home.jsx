8bca7e7035daff48687d2c2e05ebe2e68a843bd1
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2022-11-06 23:45:08 +0530

    proxy bug fixed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import LayoutApp from '../../components/Layout'
 
 const Home = () => {
+    const [productData, setProductData] = useState([]);
+
+    useEffect(() => {
+        const getAllProducts = async () => {
+            try {
+                const {data} = await axios.get('/api/products/getproducts');
+                setProductData(data);
+                console.log(data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getAllProducts();
+    }, []);
+
   return (
     <LayoutApp>
-        <h2>Home</h2>
+        Home
     </LayoutApp>
   )
 }
