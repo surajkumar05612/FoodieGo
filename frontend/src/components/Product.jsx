5c2b4316816561823ff8afda53e088525253045d
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2022-11-11 01:07:30 +0530

    updated

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 
 const Product = ({product}) => {
   const { Meta } = Card;
@@ -8,6 +9,11 @@ const Product = ({product}) => {
       hoverable
       style={{ width: 240, marginBottom: 30 }}
       cover={<img alt={product.name} src={product.image} style={{height: 200 }}/>}
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
     >
       <Meta title={product.name} description={product.category} />
     </Card>
